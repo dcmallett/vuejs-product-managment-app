f6fb8b159f91fb0d3e622f5115ce1b2ac8e1858a
dcmallett <dcmallett28@gmail.com>
2024-06-14 00:16:49 +0100

    added env file for keys and url

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  'https://rxqxtdrljuiewqwuinew.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4cXh0ZHJsanVpZXdxd3VpbmV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgzMTk2NzcsImV4cCI6MjAzMzg5NTY3N30.Z_-2Ycd8XcEY7w9g04U5YPrDJbHeCzccG4MaM1ogjpU'
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
 )
