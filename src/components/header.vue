113e90c334d0ed38969fd8db2f1e5ba3dfdb03f1
dcmallett <dcmallett28@gmail.com>
2024-07-14 23:02:41 +0100

    Added menu dropdown and input components

--- src/components/header.vue
+++ src/components/header.vue
@@ -3,15 +3,43 @@
     {{ title }}
     <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
       <form class="w-full max-w-96">
-        <input class="w-full pl-8 bg-background" type="text" placeholder="Search ...." />
+        <Search />
+        <Input class="w-full pl-8 bg-background" type="text" placeholder="Search ...." />
       </form>
-      <div class="w-8 h-8 rounded-full bg-white" />
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Avatar>
+            <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </nav>
   </header>
 </template>
 
 <script setup lang="ts">
-defineProps({
-  title: String
-})
+import Input from '@/components/ui/input/Input.vue'
+import Search from './icons/search.vue'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+defineProps<{
+  title?: string
+}>()
 </script>
