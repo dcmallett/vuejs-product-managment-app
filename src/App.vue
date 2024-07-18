8bdb8b5c999bcae16c55ea1a460a03c0eff819b7
dcmallett <dcmallett28@gmail.com>
2024-07-19 00:12:07 +0100

    Extracting nav and sidebar into thier own components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <Header title="Pulse" />
-  <main>
-    <RouterView />
-  </main>
+  <Sidebar />
+  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
+    <TopNavbar />
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
-
 <script setup lang="ts">
-import Header from './components/header.vue'
+import TopNavbar from '@/components/layout/TopNavbar.vue'
+import Sidebar from '@/components/layout/Sidebar.vue'
 </script>
